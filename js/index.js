16aa2c70b8af8a20cee10d6846bd33528e0c886c
Qoryogdiyev <asadbekqoryogdiyev7@gmail.com>
2023-04-14 15:00:07 +0500

    add edit

--- js/index.js
+++ js/index.js
@@ -26,8 +26,8 @@ getData("https://jsonplaceholder.typicode.com/users")
       <td>${name}</td>
       <td>${email}</td>
       <td>${phone}</td>
-      <td><button class="edit__btn">Edit</button></td>
-      <td><button class="delete__btn">Delete</button></td>
+      <td><button class="edit__btn" data-id=${id} data-name=${name} data-email=${email} data-phone=${phone}>Edit</button></td>
+      <td><button class="delete__btn" data-id=${id} data-name=${name} data-email=${email} data-phone=${phone}>Delete</button></td>
       `;
 
       tableBody.appendChild(row);
@@ -43,48 +43,66 @@ form.addEventListener("submit", (e) => {
   const email = document.getElementById("email").value;
   const phone = document.getElementById("phone").value;
 
-  const formData = {
-    id: id,
-    name: name,
-    email: email,
-    phone: phone,
-  };
+  if (id) {
+    updateUser(id, name, email, phone);
+    form.reset();
+  } else {
+    const formData = {
+      id: id,
+      name: name,
+      email: email,
+      phone: phone,
+    };
 
-  console.log(formData);
+    console.log(formData);
 
-  const postData = (url) => {
-    return new Promise((resolve, reject) => {
-      fetch(url, {
-        method: "POST",
-        headers: { "Content-type": "application/json" },
-        body: JSON.stringify(formData),
+    const postData = (url) => {
+      return new Promise((resolve, reject) => {
+        fetch(url, {
+          method: "POST",
+          headers: { "Content-type": "application/json" },
+          body: JSON.stringify(formData),
+        })
+          .then((response) => response.json())
+          .then((data) => resolve(data))
+          .catch((error) => reject(error));
+      });
+    };
+
+    postData("https://jsonplaceholder.typicode.com/users")
+      .then((data) => {
+        const row = document.createElement("tr");
+        row.innerHTML = `
+        <td>${data.id}</td>
+        <td>${data.name}</td>
+        <td>${data.email}</td>
+        <td>${data.phone}</td>
+        <td><button class="edit__btn" data-id=${data.id} data-name=${data.name} data-email=${data.email} data-phone=${data.phone}>Edit</button></td>
+        <td><button class="delete__btn" data-id=${data.id} data-name=${data.name} data-email=${data.email} data-phone=${data.phone}>Delete</button></td>
+        `;
+
+        tableBody.appendChild(row);
       })
-        .then((response) => response.json())
-        .then((data) => resolve(data))
-        .catch((error) => reject(error));
-    });
-  };
-
-  postData("https://jsonplaceholder.typicode.com/users")
-    .then((data) => {
-      const row = document.createElement("tr");
-      row.innerHTML = `
-      <td>${data.id}</td>
-      <td>${data.name}</td>
-      <td>${data.email}</td>
-      <td>${data.phone}</td>
-      <td><button class="edit__btn">Edit</button></td>
-      <td><button class="delete__btn">Delete</button></td>
-      `;
-
-      tableBody.appendChild(row);
-    })
-    .catch((error) => console.log(error));
+      .catch((error) => console.log(error));
+  }
 
   form.reset();
 });
 
 tableBody.addEventListener("click", (e) => {
+  if (e.target.classList.contains("edit__btn")) {
+    const id = e.target.dataset.id;
+    const name = e.target.dataset.name;
+    const email = e.target.dataset.email;
+    const phone = e.target.dataset.phone;
+
+    document.getElementById("update-id").value = id;
+    document.getElementById("name").value = name;
+    document.getElementById("email").value = email;
+    document.getElementById("phone").value = phone;
+    document.querySelector(".btn").textContent = "Update User";
+  }
+
   if (e.target.classList.contains("delete__btn")) {
     const id = e.target.dataset.id;
 
@@ -108,6 +126,39 @@ async function deleteUser(id) {
     if (response.ok) {
       return response;
     }
+  } catch {
+    (error) => console.log(error);
+  }
+}
+
+async function updateUser(id, name, email, phone) {
+  const newFormData = {
+    id: id,
+    name: name,
+    email: email,
+    phone: phone,
+  };
+
+  try {
+    const response = await fetch(
+      `https://jsonplaceholder.typicode.com/users/${id}`,
+      {
+        method: "PUT",
+        body: newFormData,
+      }
+    );
+    const data = await response.json();
+    const tableRows = document.querySelectorAll(".table__body tr");
+    console.log(tableRows);
+    for (row of tableRows) {
+      if (row.children[0].textContent == id.toString()) {
+        row.children[1].textContent = name;
+        row.children[2].textContent = email;
+        row.children[3].textContent = phone;
+      }
+    }
+    document.querySelector(".btn").textContent = "Save";
+    return data;
   } catch {
     (error) => console.log(error);
   }
